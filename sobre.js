42b66900077ea2b0e7a6c596617512059603337d
Kaique Baccas <kaiquebaccas02@gmail.com>
2024-08-17 15:20:17 -0300

    Página inicial e Sobre.
    Corrigi o upload do Kauan.

---
+++ sobre.js
@@ -0,0 +1,18 @@
+const maises = document.querySelectorAll(".mais")
+
+for(const mais of maises){
+
+    mais.addEventListener("click",(e) => 
+    {
+        console.log(mais)
+
+    });
+
+    
+
+
+
+
+
+
+}
