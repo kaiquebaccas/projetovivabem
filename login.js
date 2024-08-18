82b9b6cc9140deca420b4d460b6d4df153244013
Kaique Baccas <kaiquebaccas02@gmail.com>
2024-08-18 19:56:12 -0300

    Página Contato atualizada

--- login.js
+++ login.js
@@ -12,15 +12,3 @@ loginLink.addEventListener('click', () => {
   registerForm.style.display = 'none';
   loginForm.style.display = 'block';
 });
-
-/* vou ver isso amanha, mas é pra mostrar/ocultar senha 
-
-function togglePassword() {
-    const passwordField = document.getElementById('password');
-    const passwordFieldType = passwordField.type;
-    if (passwordFieldType === 'password') {
-        passwordField.type = 'text';
-    } else {
-        passwordField.type = 'password';
-    }
-} */ 
