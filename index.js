fbdaa28768b6a9a06e9d9196e9b2f77eaa9ecaba
Kauan S. Lima <stranothomas00@gmail.com>
2024-09-16 01:08:55 -0300

    Atualização JS - Pop-up e Cookies

--- index.js
+++ index.js
@@ -74,3 +74,79 @@ document.getElementById('toggle-tema').addEventListener('click', function() {
   const p = document.querySelector('p');
   p.classList.toggle('dark-tema')
 });
+
+// espera até que todo o documento seja carregado
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+// POP-UP ---------------------------------------------------------------------------------------
+const popup = document.getElementById('coki');
+const novamente = document.getElementById('novamente');
+const fechar = document.getElementById('fechar');
+const tudo = document.getElementById('modal');
+
+// define cookies
+
+function setcookie(name, value, days){
+      // define a data de expiração e converte pro formato universal
+  const date = new Date();
+  date.setTime(date.getTime + (days * 24 * 60 * 60 * 1000)); //date.SETTime define a data e a hora (em milisegundos desde 1970). date GET Time define a data atual em milissegundos desde 1970
+ // days * tanana calcula em milissegundos os dias fornecidos para expirar o cookie
+  let expira = 'expires=' + date.toUTCString();
+      // cria a string que define o cookie com nome, valor e data de expiração
+  document.cookie = name + '=' + (value || '') + ';' + expira + ";path=/" //path indica que é valido em todo caminho começado com "/", ou seja, o site todo
+
+}
+
+function getcookie(name){
+  let nameehigual = name + "="
+
+  let arraydecoki = document.cookie.split(';') // divide cada cookie como uma posição na array, usando ';' pra delimitar
+
+  for(x = 0; x < arraydecoki.length; x++) //ou seja, para cada cookie na array de cookies
+  {
+    let cookie = arraydecoki[x];
+
+    while(cookie.charAt(0) === ' ') //enquanto a primeira letra de cookie for vazia, ...
+    {
+      cookie = cookie.substring(1) // cookie vai valer ele mesmo, sem a posição 0, para evitar espaços vazios antes de ';'
+    };
+
+    if(cookie.indexOf(nameehigual) === 0) // se nameehigual for encontrado em cookie na posição 0, ou seja, se os nomes forem os mesmos, ...
+      {
+        return cookie.substring(nameehigual.length, cookie.length) // retorna o valor do cookie, que é a substring cortando a parte que eles são os mesmos até o final de cookie
+      }
+
+      return null; // se nada disso acontecer, e o cookie n existir, retorna nula
+
+  }
+
+}
+
+function checkcookie(){
+  const popupado = getcookie('popupShown') //busca o valor retornado pela getcookie, 'popupShown' se for encontrado ou null se não, e armazena esse valor na popupado
+
+  if(popupado === 'true') // se popupado encontrou valor true
+  {
+    return; //não mostra o popup, pq o usuario ja optou por não ve-lo
+  }
+  popup.showModal();          //\
+  tudo.style.display = 'block'; // \___ se não, exibe o popup e o "tudo", que cria um fundo atrás dele
+
+}
+
+function fecharpopup(){
+  if(novamente.checked) //se a caixinha estiver selecionada
+    {
+    setcookie('popupShown', 'true', 365) // define um cookie com o nome popupshown e o valor true
+  }
+  popup.close();                // fecha o popup
+  tudo.style.display = 'none'; // fecha o tudo
+}
+
+  checkcookie(); // chama a função checkcookie quando a pág é carregada
+
+  fechar.addEventListener('click', fecharpopup); // ao clicar no butao p fechar, ele fecha (só garantindo que vou explicar tudo)
+
+
+});
