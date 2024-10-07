755173a789776bdca55f7fa21532a04c1dc4e80c
Igor-Lima-Jesus <114624078+IgorFax08@users.noreply.github.com>
2024-10-07 12:00:48 -0300

    API GEOLOCALIZAÇAÕ

--- JS/index.js
+++ JS/index.js
@@ -149,3 +149,13 @@ function fecharpopup(){
 
 
 });
+
+var map = L.map('map').setView([-24.008779479675535, -46.43470060794742], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
